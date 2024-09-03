80edde66f6670590f40acfe795c34dbdc72a82e8
Warren James <warren.james@mongodb.com>
2024-09-03 12:04:07 -0400

    remove unneeded init

--- lua/config/orgmode/init.lua
+++ lua/config/orgmode/init.lua
@@ -1,7 +1,6 @@
 local orgmode = require('orgmode')
 local M = {
   setup = function()
-    orgmode.setup_ts_grammar()
     orgmode.setup({
       org_agenda_files = {'~/notes/org/*'},
       org_default_notes_file = '~/notes/org/notes.org',
