f69bc061513657af41ea07e6c2cd8a4afae547ae
Warren James <warren.james@mongodb.com>
2024-09-03 12:02:26 -0400

    update dap config

--- lua/config/dap/node.lua
+++ lua/config/dap/node.lua
@@ -8,14 +8,9 @@ function M.setup()
     adapters = { 'pwa-node', 'node-terminal' }
   })
 
-  dap.adapters.node2 = {
-    type = 'executable',
-    command = 'node',
-    args = { os.getenv('HOME') .. '/tools/vscode-node-debug2/out/src/nodeDebug.js' },
-  }
-
   local function get_mocha_pid()
-    local output = io.popen("ps ah | sed --quiet --regexp-extended '{s/([0-9]+).*npm exec mocha.*inspect(-brk)?/\1/; /^[0-9]+$/p}'")
+    local output = io.popen(
+      "ps ah | sed --quiet --regexp-extended '{s/ *([0-9]+).*npm exec mocha.*inspect(-brk)?/\1/; /^[0-9]+$/p}'")
     local pid
     if output ~= nil then
       pid = output:read("*a")
@@ -26,45 +21,92 @@ function M.setup()
     end
   end
 
-  dap.configurations.typescript = {
-    {
-      name = 'Attach to process',
-      type = 'pwa-node',
-      request = 'attach',
-      protocol = 'inspector',
-      processId = dap_utils.pick_process,
-      console = 'integratedTerminal',
-      internalConsoleOptions = 'neverOpen',
-      justMyCode = false,
-    },
-    {
-      name = 'Attach to Mocha',
-      type = 'pwa-node',
-      request = 'attach',
-      sourceMaps = true,
-      protocol = 'inspector',
-      processId = get_mocha_pid,
-      console = 'integratedTerminal',
-      internalConsoleOptions = 'neverOpen',
-      justMyCode = false,
-    },
-    {
-      name = 'Launch with Mocha',
-      type = 'pwa-node',
-      request = 'launch',
-      protocol = 'inspector',
-      trace = true,
-      runtimeExecutable = 'npx',
-      runtimeArgs = { 'mocha', '${file}' },
-      rootPath = '${workspaceFolder}',
-      cwd = '${workspaceFolder}',
-      console = 'integratedTerminal',
-      internalConsoleOptions = 'neverOpen',
-      justMyCode = false,
-    }
-  }
+  local function get_node_pid()
+    --local output = io.popen(
+    --"ps ah | sed --quiet --regexp-extended '{s/ *([0-9]+).*node .*--inspect(-brk)?.*/\1/; /^[0-9]+$/p}'")
+    local output = io.popen(
+      "ps ah | awk -F ' ' '/node.* --inspect(-brk)/ && $5 != \"awk\" && $5 != \"gawk\" {print $1}'")
+    local pid
+    if output ~= nil then
+      pid = output:read("*a")
+      output:close()
+      return pid
+    else
+      return -1
+    end
+  end
 
-  dap.configurations.javascript = dap.configurations.typescript
+  for _, lang in ipairs({ 'typescript', 'javascript' }) do
+    dap.configurations[lang] = {
+      {
+        name = 'Attach to process',
+        type = 'pwa-node',
+        request = 'attach',
+        protocol = 'inspector',
+        processId = dap_utils.pick_process,
+        console = 'integratedTerminal',
+        internalConsoleOptions = 'neverOpen',
+        justMyCode = false,
+      },
+      {
+        name = 'Attach to Mocha',
+        type = 'pwa-node',
+        request = 'attach',
+        sourceMaps = true,
+        protocol = 'inspector',
+        continueOnAttach = true,
+        processId = get_mocha_pid,
+        console = 'integratedTerminal',
+        internalConsoleOptions = 'neverOpen',
+        justMyCode = false,
+      },
+      {
+        name = 'Attach to Node',
+        type = 'pwa-node',
+        request = 'attach',
+        sourceMaps = true,
+        protocol = 'inspector',
+        continueOnAttach = true,
+        processId = get_node_pid,
+        console = 'integratedTerminal',
+        internalConsoleOptions = 'neverOpen',
+        justMyCode = false,
+      },
+      {
+        name = 'Launch unit test with Mocha',
+        type = 'pwa-node',
+        request = 'launch',
+        --protocol = 'inspector',
+        runtimeExecutable = 'node',
+        runtimeArgs = { '--inspect', '--no-lazy', '--require', 'ts-node/register/transpile-only', './node_modules/mocha/bin/mocha.js', '${file}' },
+        rootPath = '${workspaceFolder}',
+        cwd = '${workspaceFolder}',
+        console = 'integratedTerminal',
+        internalConsoleOptions = 'neverOpen',
+      },
+      {
+        name = 'Launch integration test with Mocha',
+        type = 'pwa-node',
+        request = 'launch',
+        --protocol = 'inspector',
+        runtimeExecutable = 'node',
+        runtimeArgs = { '--inspect', '--no-lazy', '--require', 'ts-node/register/transpile-only', './node_modules/mocha/bin/mocha.js', '--config', './test/mocha_mongodb.json', '${file}' },
+        rootPath = '${workspaceFolder}',
+        cwd = '${workspaceFolder}',
+        console = 'integratedTerminal',
+        internalConsoleOptions = 'neverOpen',
+      },
+      {
+        name = 'Launch with ts-node',
+        type = 'pwa-node',
+        request = 'launch',
+        runtimeExecutable = 'node',
+        runtimeArgs = { '--inspect', '--no-lazy', '--require', 'ts-node/register/transpile-only' },
+        cwd = '${workspaceFolder}',
+        program = '${file}',
+      }
+    }
+  end
 end
 
 return M
