2099c7aa2c4bddfe7613303a111353c08e5484ce
W-A-James <warren.james.dev@gmail.com>
2024-09-28 03:13:53 -0400

    update theming

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -6,16 +6,19 @@ local G = require("config.utils").G
 function M.setup()
   ------------------------Gruvbox material theming--------------------------------
   set.background = 'dark'
-  G['gruvbox_material_background'] = 'hard'
+  G['gruvbox_material_background'] = 'soft'
   G['gruvbox_material_better_performance'] = '1'
   G['gruvbox_material_enable_bold'] = '1'
   G['gruvbox_material_enable_italic'] = '1'
-  G['gruvbox_material_transparent_background'] = '0'
+  G['gruvbox_material_transparent_background'] = '1'
 
-  vim.cmd "colorscheme github_dark_dimmed"
+  vim.cmd "colorscheme gruvbox-material"
 
   -- Lualine setup
   require('lualine').setup()
+
+  -- Rainbow delimiters
+  require('rainbow-delimiters')
 end
 
 return M
